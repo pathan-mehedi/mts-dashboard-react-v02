8a47cdfebbb337333a681581bb33431b49689fae
Mehedi Pathan <sma.mehedi@gmai.com>
2023-08-03 18:40:13 +0600

    Initial commit

--- src/components/dataTable/DataTable.tsx
+++ src/components/dataTable/DataTable.tsx
@@ -5,7 +5,6 @@ import {
 } from "@mui/x-data-grid";
 import "./dataTable.scss";
 import { Link } from "react-router-dom";
-// import { useMutation, useQueryClient } from "@tanstack/react-query";
 
 type Props = {
   columns: GridColDef[];
@@ -15,21 +14,9 @@ type Props = {
 
 const DataTable = (props: Props) => {
 
-  // TEST THE API
 
-  // const queryClient = useQueryClient();
-  // // const mutation = useMutation({
-  // //   mutationFn: (id: number) => {
-  // //     return fetch(`http://localhost:8800/api/${props.slug}/${id}`, {
-  // //       method: "delete",
-  // //     });
-  // //   },
-  // //   onSuccess: ()=>{
-  // //     queryClient.invalidateQueries([`all${props.slug}`]);
-  // //   }
-  // // });
 
-  const handleDelete = (id: number) => {
+  const handleDelete = (_id: number) => {
     //delete the item
     // mutation.mutate(id)
   };
